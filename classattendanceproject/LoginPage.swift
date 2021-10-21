73191ae21eebee043d203de802a9cdb33c5401dd
Parisut Supchokpool <paintpaintpaint@MacBook-Pro-khxng-Parisut.local>
2021-10-22 03:15:07 +0700

    loginbutnofuckinganything

---
+++ classattendanceproject/LoginPage.swift
@@ -0,0 +1,39 @@
+//
+//  LoginPage.swift
+//  classattendanceproject
+//
+//  Created by Parisut Supchokpool on 22/10/2564 BE.
+//
+
+import UIKit
+import Firebase
+class LoginPage: UIViewController {
+
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    
+    @IBAction func loginButton(_ sender: Any) {
+
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
